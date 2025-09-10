6cef3d3cf21e2cdc344eed39a4c3002d0685d9db
bibashmanjusubedi <acharabibash@outlook.com>
2025-09-10 22:44:35 +0545

    first commit: hello world

---
+++ Assignments/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Hello world. I am Bibash Acharya. I am currently Interning at Amnil Technologies. ");
+Console.Write("Coding is fun and I am passionate to learn new technologies. ");
+Console.Write("I have been programming since 6 years.My first computer program was a Hello World in QBASIC. ");
+Console.Write("All my projects are available at https://github.com/bibashmanjusubedi ");
+Console.Write("My programming skills include C#, Python , JavaScript.");
+Console.Write("Programming was a just a hobby when I was a child but since I studied and have decided to pursue a career in Computer Scince,it has become a part of my life. ");
+Console.Write("When I am not opening my text editor you can find me solving competitive programming problems on  sites like LeetCode and Hackerank. ");
+Console.Write("One of my recent projects is a Full Stack Asset Management System Web Application on ASP.NET Backend and ReactJS frontend. ");
+Console.Write("I want to be a full stack developer but currently I am focusing on my frontend skills like making websites responsive. ");
+Console.Write("I am also learning Web Design with Figma to advance my frontend skills. ");
+Console.Write("On the backend side, I am getting to be famniliar with MS-SQL database as my recent project made use of ADO.NET");

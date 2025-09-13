492f31ec63a8943a822992886261387edd09e9f3
bibashmanjusubedi <acharabibash@outlook.com>
2025-09-13 14:15:06 +0545

    Assignment 3

---
+++ Assignment3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter your city: ");
+        string city = Console.ReadLine();
+        Console.Write("Enter your favorite hobby: ");
+        string hobby = Console.ReadLine();
+        DisplayUserInfo(name,age,city,hobby);
+    }
+    static void DisplayUserInfo(string name,int age,string city,string hobby)
+    {
+        int birthYear = DateTime.Now.Year - age;
+        Console.WriteLine($"\n User Information: ");
+        Console.WriteLine($"Name:   {name}");
+        Console.WriteLine($"Age:    {age}");
+        Console.WriteLine($"City:   {city}");
+        Console.WriteLine($"Hobby:  {hobby}");
+        Console.WriteLine($"Birth Year: {birthYear}");
+    }
+}
